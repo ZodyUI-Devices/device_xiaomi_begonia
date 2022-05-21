fc81786b98212058b7bf6c75061ae84a893377f3
AmeChanRain <AmeChanRain@users.noreply.github.com>
2022-05-21 01:51:25 +0000

    miku: Set Maintainer informations for Miku UI

--- device.mk
+++ device.mk
@@ -279,7 +279,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-miku
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
